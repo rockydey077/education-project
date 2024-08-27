d1504d2a063567519ebacfbd319664815c9bf8d5
rockydey <rockydey077@gmail.com>
2024-08-27 17:55:10 +0600

    latest news design complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/HomeComponents/Banner";
 import ChooseUs from "@/components/HomeComponents/ChooseUs";
+import LatestNews from "@/components/HomeComponents/LatestNews";
 
 export default function Home() {
   return (
     <main className="bg-[#FFF7F7]">
       <Banner />
+      <LatestNews />
       <ChooseUs />
     </main>
   );
---
+++ src/components/HomeComponents/LatestNews.jsx
@@ -0,0 +1,27 @@
+import Marquee from "react-fast-marquee";
+
+const LatestNews = () => {
+  return (
+    <div className="max-w-[1360px] mx-auto p-[10px] flex items-center gap-3">
+      <div className="text-[#0055A4] uppercase text-[30px] font-semibold">
+        SIMT
+      </div>
+      <div className="text-[#ff0000] text-2xl rubik font-medium">
+        <Marquee className="max-w-[265px] lg:max-w-[1260px]">
+          <p className="whitespace-nowrap">
+            Admission Going on into 23rd Batch of 4-year Diploma in Engineering
+            Program in the session-2024-2025. Hotline: 01936-005817,
+            01936-005818...&nbsp;
+          </p>
+          <p className="whitespace-nowrap">
+            Admission Going on into 23rd Batch of 4-year Diploma in Engineering
+            Program in the session-2024-2025. Hotline: 01936-005817,
+            01936-005818...&nbsp;
+          </p>
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default LatestNews;
