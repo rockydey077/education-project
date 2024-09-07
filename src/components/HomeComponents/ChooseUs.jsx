04cb8d21a440548694d484dea639ea999766c9e2
rockydey <rockydey077@gmail.com>
2024-09-07 11:38:49 +0600

    choose up section updated

--- src/components/HomeComponents/ChooseUs.jsx
+++ src/components/HomeComponents/ChooseUs.jsx
@@ -11,23 +11,24 @@ const ChooseUs = () => {
   return (
     <div className="max-w-[1360px] mx-auto py-[50px] space-y-[50px] px-2 lg:px-0">
       <div>
-        <h1 className="text-[#0055A4] text-[30px] rubik lg:text-[50px] font-semibold uppercase text-center mb-6">
-          Why Choose SIMT?
+        <h1 className="text-[#2D3192] text-[30px] rubik lg:text-[50px] font-semibold uppercase text-center mb-6">
+          Why Choose DIET?
         </h1>
         <p className="rubik text-base lg:text-[25px] font-normal leading-[36px]">
-          Saic Institute of Management and Technology (SIMT) is one of the best
-          polytechnic institute in Dhaka, offering comprehensive programs in
-          Diploma Engineering. At SIMT, we prioritize providing unwavering
-          support and cultivating an enriching learning environment. Our
-          institute is dedicated to equipping you with the knowledge, skills,
-          confidence, and experience necessary to make a positive impact on the
-          world.
+          Choosing Dhaka Institute of Engineering & Technology (DIET) offers
+          affordable, industry-focused technical education with hands-on
+          training in fields like Electrical, Civil, and Computer Engineering.
+          Located in Dhaka, DIET provides access to job markets, internships,
+          and practical experiences, preparing students for employment. Smaller
+          class sizes ensure personalized learning, and the curriculum is
+          aligned with industry needs, making DIET a strong option for students
+          pursuing engineering and vocational careers.
         </p>
       </div>
       <div
-        className={`${styles.choose_shadow} space-y-[14px] bg-white border-l-4 border-[#0055A4] py-[37px] px-[43px]`}
+        className={`${styles.choose_shadow} space-y-[14px] bg-white border-l-4 border-[#2D3192] py-[37px] px-[43px]`}
       >
-        <div className="bg-[#0055A4] w-fit text-white text-2xl p-3 rounded-full">
+        <div className="bg-[#2D3192] w-fit text-white text-2xl p-3 rounded-full">
           <FaQuoteLeft />
         </div>
         <p
@@ -37,7 +38,7 @@ const ChooseUs = () => {
           more and become better in their respective life journeys.
         </p>
         <p className="uppercase text-xl font-semibold rubik text-[#707070]">
-          - Principal, SIMT
+          - Principal, DIET
         </p>
       </div>
     </div>
