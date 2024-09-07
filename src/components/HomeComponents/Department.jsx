b75f068790dc9426f0aecad95fad9fe40e70b90b
rockydey <rockydey077@gmail.com>
2024-09-07 11:36:22 +0600

    Department section updated

--- src/components/HomeComponents/Department.jsx
+++ src/components/HomeComponents/Department.jsx
@@ -6,11 +6,11 @@ import { FaLaptopCode } from "react-icons/fa6";
 const Department = () => {
   return (
     <div className="max-w-[1360px] mx-auto py-[50px] space-y-5 px-2 lg:px-0">
-      <h1 className="text-[#0055A4] rubik text-[30px] lg:text-[50px] font-semibold uppercase text-center">
+      <h1 className="text-[#2D3192] rubik text-[30px] lg:text-[50px] font-semibold uppercase text-center">
         Our Department
       </h1>
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-5 lg:flex-wrap">
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+        <div className={`rounded border-2 border-[#2D3192]`}>
           <div
             className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
           >
@@ -21,12 +21,12 @@ const Department = () => {
               <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
                 <FaLaptopCode />
               </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
+              <h3 className="text-[#2D3192] bg-whiterubik text-xl font-semibold uppercase pb-10">
+                Computer Engineering
               </h3>
               <div className="">
                 <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
+                  className={`hover:bg-white hover:text-[#2D3192] border border-[#2D3192] px-5 py-[10px] rounded-full text-white bg-[#2D3192] rubik text-base font-medium`}
                 >
                   Show More
                 </button>
@@ -34,7 +34,7 @@ const Department = () => {
             </div>
           </div>
         </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
+        <div className={`rounded border-2 border-[#2D3192]`}>
           <div
             className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
           >
@@ -45,12 +45,12 @@ const Department = () => {
               <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
                 <FaLaptopCode />
               </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
+              <h3 className="text-[#2D3192] bg-whiterubik text-xl font-semibold uppercase pb-10">
+                Civil Engineering
               </h3>
               <div className="">
                 <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
+                  className={`hover:bg-white hover:text-[#2D3192] border border-[#2D3192] px-5 py-[10px] rounded-full text-white bg-[#2D3192] rubik text-base font-medium`}
                 >
                   Show More
                 </button>
@@ -58,7 +58,7 @@ const Department = () => {
             </div>
           </div>
         </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
+        <div className={`rounded border-2 border-[#2D3192]`}>
           <div
             className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
           >
@@ -69,12 +69,12 @@ const Department = () => {
               <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
                 <FaLaptopCode />
               </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
+              <h3 className="text-[#2D3192] bg-whiterubik text-xl font-semibold uppercase pb-10">
+                Electrical Engineering
               </h3>
               <div className="">
                 <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
+                  className={`hover:bg-white hover:text-[#2D3192] border border-[#2D3192] px-5 py-[10px] rounded-full text-white bg-[#2D3192] rubik text-base font-medium`}
                 >
                   Show More
                 </button>
@@ -82,7 +82,7 @@ const Department = () => {
             </div>
           </div>
         </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
+        <div className={`rounded border-2 border-[#2D3192]`}>
           <div
             className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
           >
@@ -93,180 +93,12 @@ const Department = () => {
               <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
                 <FaLaptopCode />
               </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
+              <h3 className="text-[#2D3192] bg-whiterubik text-xl font-semibold uppercase pb-10">
+                Electronics Engineering
               </h3>
               <div className="">
                 <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
-                >
-                  Show More
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={`lg:w-[23%] rounded border-2 border-[#0055A4]`}>
-          <div
-            className={`h-[300px] ${styles.card} z-10 relative overflow-hidden cursor-pointer`}
-          >
-            <Image src={computer} width={400} height={400} alt="" />
-            <div
-              className={` flex flex-col items-center w-full absolute bottom-0 h-[80px] duration-500 ${styles.card_content} bg-white`}
-            >
-              <div className="text-4xl text-[#ff0000] p-5 rounded-full bg-white -mt-10">
-                <FaLaptopCode />
-              </div>
-              <h3 className="text-[#0055A4] bg-whiterubik text-xl font-semibold uppercase pb-10">
-                Computer Science
-              </h3>
-              <div className="">
-                <button
-                  className={`hover:bg-white hover:text-[#0055A4] border border-[#0055A4] px-5 py-[10px] rounded-full text-white bg-[#0055A4] rubik text-base font-medium`}
+                  className={`hover:bg-white hover:text-[#2D3192] border border-[#2D3192] px-5 py-[10px] rounded-full text-white bg-[#2D3192] rubik text-base font-medium`}
                 >
                   Show More
                 </button>
